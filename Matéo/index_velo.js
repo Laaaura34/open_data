8b1e1d594bf02db6c6896f2e6af8fe4e575fe9f9
mateocalsacy <mateo.calsacy@etu.univ-montp3.fr>
2022-11-03 14:41:50 +0100

    code data vélo

--- Matéo/index_velo.js
+++ Matéo/index_velo.js
@@ -12,10 +12,10 @@ const puppeteer = require('puppeteer');
 
 
 
-app.get('/energie', function(request, response){
+app.get('/velo', function(request, response){
     const code_insee = request.query.code_insee;
 
-    const file = XLSX.readFile('conso-elec-gaz-annuelle-par-secteur-dactivite-agregee-commune.xlsx')
+    const file = XLSX.readFile('velo.xlsx')
     const files = file.SheetNames //Récupération des noms de feuilles du fichier excel : ici nous n'en n'avons qu'une seule
     const insee = file.Sheets[files[0]]; //Récupération de la première et unique feuille du fichier excel pour pouvoir parser les données.
 
@@ -26,13 +26,11 @@ app.get('/energie', function(request, response){
     var tableau = []
         //Création d'une boucle qui rempli le tableau json crée avec les données dont ont à besoin.
         //On itère la boucle sur le nombre total d'observation dans le but de récupérer dans un format json les données.
-    for (let i = tab; i < 5000; i++) {
+    for (let i = tab; i < 1000; i++) {
         var json1 = {}; // Création d'une variable au format json
         json1['code_insee'] = insee['A' + i]['v']; // On créer un tableau json vide en récupérant comme identifiant de chaque tableau : le code insee. Création d'un identifiant unique.
-        json1['code_postal'] = insee['B' + i]['v']; // On rempli le tableau json de la ligne i avec l'observation du bureau de vote,
-        json1['commune'] = insee['C' + i]['v']; // avec le nom des départements,
-        json1['conso_totale'] = insee['D' + i]['v']; // avec le nb d'inscrit,
-
+        json1['capacite'] = insee['B' + i]['v']; // On rempli le tableau json de la ligne i avec l'observation du bureau de vote,
+        
         if (json1['code_insee'] == code_insee) tableau = [...tableau, json1]
         else {
             if (code_insee == null) tableau = [...tableau, json1]
