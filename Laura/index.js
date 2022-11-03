8020bb4939236f2f1e37e7b8fd57d39ee5ebdd8b
Laaaura34 <laura.senecaille@gmail.com>
2022-11-03 09:18:12 +0100

    Màj index covoit

--- Laura/index.js
+++ Laura/index.js
@@ -7,7 +7,6 @@ const PORT = process.env.PORT || 3001;
 
 app.get('/', function(request, response){
     const codeInsee = request.query.codeInsee;
-
     const file = XLSX.readFile('./data/covoit.xlsx')
 
     const files = file.SheetNames //Récupération des noms de feuilles du fichier excel : ici nous n'en n'avons qu'une seule mais je n'ai pas trouvée le moyen de faire autrement
@@ -20,13 +19,13 @@ app.get('/', function(request, response){
     var tableau = []
         //Création d'une boucle qui rempli le tableau json crée avec les données dont ont à besoin.
         //On itère la boucle sur le nombre total d'observation dans le but de récupérer dans un format json les données.
-    for (let i = tab; i < 5000; i++) {
+    for (let i = tab; i < 1000; i++) {
         var json1 = {}; // Création d'une variable au format json
-		json1['codeInsee'] = covoit['F' + i]["v"]; 
-		json1['Communes'] = covoit['E' + i]['v']; 
-		//json1['Type'] = covoit['G' + i];
-		//json1['Longitude'] = covoit['K' + i]; 
-		//json1['Latitude'] = covoit['L' + i]; 
+		json1['insee'] = covoit['B' + i]['v']; 
+		json1['com_lieu'] = covoit['A' + i]['v']; 
+		json1['type'] = covoit['C' + i]['v'];
+		json1['Xlong'] = covoit['D' + i]['v']; 
+		json1['Xlat'] = covoit['E' + i]['v']; 
 
         if (json1['codeInsee'] == codeInsee) tableau = [...tableau, json1]
         else {
