ef59efbccf05e37fe492f287d762362f17b72313
zaineb KHARFATI <zaineb_kharfati@outlook.fr>
2022-11-26 15:30:58 +0100

    update
    commit

---
+++ Zaineb/aname.js
@@ -0,0 +1,60 @@
+
+
+"use strict";
+
+var express = require("express") /* npm install express */
+const app = express();
+const axios = require('axios');
+const PORT = process.env.PORT || 3001;
+
+
+
+function train(){
+
+    var data = []
+    return axios
+        .get(
+            'https://data.sncf.com/api/records/1.0/search/?dataset=lignes-equipees-de-liaison-radio-sol-train&q=&rows=10000&facet=code_ligne&facet=rst'
+        )
+        .then(res => {
+            //const code_commune_insee = request.query.code_commune_insee;
+
+            res['data']['records'].forEach((element) => {
+                const obj = {}
+                obj['lib_ligne'] = element['fields']['lib_ligne']
+                obj['x_f_wgs84'] = element['fields']['x_f_wgs84']
+                obj['y_f_wgs84'] = element['fields']['y_f_wgs84']
+                obj['coordinates'] = element['fields']['geo_shape']['coordinates']
+
+               // if (obj['code_commune_insee'] == code_commune_insee) data = [...data, obj]
+                //else {
+                  //  if (code_commune_insee == null) 
+                data = [...data, obj]
+               // }
+                
+
+
+                //}
+            });
+            return (data);
+
+        });
+    }
+
+    app.get('/join', function(request, response){
+        train().then(function(data){
+            const arr1 = [...data]
+            const arr2= arr1.map(arr1 => arr1.coordinates);
+            //const arr2= arr1.from(['coordinates'])
+            JSON.stringify(arr2);
+            response.send(arr2);
+
+    })
+})
+
+
+
+
+  app.listen(PORT, function(){
+	console.log('Bienvenu sur le port :'+ PORT);
+})
